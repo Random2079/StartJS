6d4febfb09fbca05d9fad06b53312ad8dc8be1c8
KEE <tean8282@gmail.com>
2024-11-26 12:58:34 +0500

    2project

--- Октябрь/27.10.24/2project/main.js
+++ Октябрь/27.10.24/2project/main.js
@@ -1,4 +1,27 @@
 window.onload = function() {
-    let icon = this.document.querySelector
+    const icon = document.querySelector('.sidebar__icon')
+
+    const main = document.querySelector('.main')
+
+    const main_content = document.querySelector('.main > .main__content')
+
+    icon.onclick = function(){
+        this.classList.toggle('change-icon')
+        main.classList.toggle('main_bg') 
+        main_content.classList.toggle('main__content_bg')
+    };    
+
+    let closeMenu = event => {
+        let target = event.target;4        
+
+        if(target.classList.contains('main_bg')){
+            icon.classList.remove('change-icon')
+            main.classList.remove('main_bg') 
+            main_content.classList.remove('main__content_bg')
+        }
+    }
+
+
+    main.addEventListener('click',closeMenu)
 }
 
